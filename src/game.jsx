67e658b088bae22572f531bf351ebd3f6ed32707
Ian Obermiller <iano@fb.com>
2014-08-30 15:17:19 -0700

    Switch between Board and Wordform without hiding
    Instead of using classes to hide the Board or Wordform components,
    conditionally display one or the other based on this.state.playing.

--- src/game.jsx
+++ src/game.jsx
@@ -2,28 +2,31 @@
 
 var Game = React.createClass({
   getInitialState: function(){
-    return {playing: false,tiles:[]};
+    return {playing: false, tiles:[]};
   },
+
   startGame: function(words){
     this.setState({
-      tiles:_.shuffle(words.concat(words)),
-      playing:true,
-      seed:Math.random()
+      tiles: _.shuffle(words.concat(words)),
+      playing: true,
+      seed: Math.random()
     });
   },
+
   endGame: function(){
     this.setState({playing:false});
   },
+
   render: function() {
-    return (
-      <div>
-        <div className={this.state.playing ? "showing" : "hidden"}>
-          <Board endGame={this.endGame} tiles={this.state.tiles} max={this.state.tiles.length/2} key={this.state.seed}/>
-        </div>
-        <div className={this.state.playing ? "hidden" : "showing"}>
-          <Wordform startGame={this.startGame} />
-        </div>
-      </div>
+    return this.state.playing ? (
+      <Board
+        endGame={this.endGame}
+        tiles={this.state.tiles}
+        max={this.state.tiles.length / 2}
+        key={this.state.seed}
+      />
+    ) : (
+      <Wordform startGame={this.startGame} />
     );
   }
 });
